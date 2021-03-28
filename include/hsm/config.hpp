f18a2f8ff23a11535cc6811f42bea82876cc8ccd
Andreas Pokorny <andreas.pokorny@gmail.com>
2021-03-28 17:52:16 +0200

    Disable non type template parameter for now
    This change needs more work, before we can remove the legacy template
    string support

--- include/hsm/config.hpp
+++ include/hsm/config.hpp
@@ -8,7 +8,7 @@
 #define HSM_CONFIG_HPP_INCLUDED
 #include <version>
 
-#if defined(__cpp_nontype_template_parameter_class) && defined(__cpp_impl_three_way_comparison)
+#if 0 // defined(__cpp_nontype_template_parameter_class) && defined(__cpp_impl_three_way_comparison)
 #define HSM_USE_PROPER_LITERALS 1
 #define HSM_TEMPLATE_LITERAL(x) template<str_lit x>
 #else
